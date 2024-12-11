19344fce1c3c831586052beaae6e6db4055b18e1
Sepidehmasoumi <129841560+Sepidehmasoumi@users.noreply.github.com>
2024-12-11 20:20:36 +0330

    Add files via upload

---
+++ SOLID/SOLID/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace SOLID
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbshapeType = new System.Windows.Forms.ComboBox();
+            this.txtvalue1 = new System.Windows.Forms.TextBox();
+            this.btncalculate = new System.Windows.Forms.Button();
+            this.lbresult = new System.Windows.Forms.Label();
+            this.txtvalue2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbshapeType
+            // 
+            this.cmbshapeType.FormattingEnabled = true;
+            this.cmbshapeType.Location = new System.Drawing.Point(84, 67);
+            this.cmbshapeType.Name = "cmbshapeType";
+            this.cmbshapeType.Size = new System.Drawing.Size(234, 28);
+            this.cmbshapeType.TabIndex = 0;
+            // 
+            // txtvalue1
+            // 
+            this.txtvalue1.Location = new System.Drawing.Point(84, 147);
+            this.txtvalue1.Name = "txtvalue1";
+            this.txtvalue1.Size = new System.Drawing.Size(228, 27);
+            this.txtvalue1.TabIndex = 1;
+            // 
+            // btncalculate
+            // 
+            this.btncalculate.Location = new System.Drawing.Point(145, 275);
+            this.btncalculate.Name = "btncalculate";
+            this.btncalculate.Size = new System.Drawing.Size(122, 29);
+            this.btncalculate.TabIndex = 2;
+            this.btncalculate.Text = "محاسبه مساحت";
+            this.btncalculate.UseVisualStyleBackColor = true;
+            this.btncalculate.Click += new System.EventHandler(this.btncalculate_Click);
+            // 
+            // lbresult
+            // 
+            this.lbresult.AutoSize = true;
+            this.lbresult.Location = new System.Drawing.Point(154, 319);
+            this.lbresult.Name = "lbresult";
+            this.lbresult.Size = new System.Drawing.Size(0, 20);
+            this.lbresult.TabIndex = 3;
+            // 
+            // txtvalue2
+            // 
+            this.txtvalue2.Location = new System.Drawing.Point(84, 221);
+            this.txtvalue2.Name = "txtvalue2";
+            this.txtvalue2.Size = new System.Drawing.Size(228, 27);
+            this.txtvalue2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(126, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "شکل خود را انتخاب کنید";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(264, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "  طول مستطیل یا شعاع دایره را وارد کنید";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(109, 189);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(174, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "عرض مستطیل را وارد کنید";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 364);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtvalue2);
+            this.Controls.Add(this.lbresult);
+            this.Controls.Add(this.btncalculate);
+            this.Controls.Add(this.txtvalue1);
+            this.Controls.Add(this.cmbshapeType);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbshapeType;
+        private System.Windows.Forms.TextBox txtvalue1;
+        private System.Windows.Forms.Button btncalculate;
+        private System.Windows.Forms.Label lbresult;
+        private System.Windows.Forms.TextBox txtvalue2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SOLID/SOLID/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SOLID
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            cmbshapeType.Items.Add("مستطیل");
+            cmbshapeType.Items.Add("دایره");
+            cmbshapeType.SelectedIndex = 0;
+        }
+
+        private void btncalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Shape shape = null;
+                if (cmbshapeType.SelectedItem.ToString()=="مستطیل")
+                {
+                    double length = double.Parse(txtvalue1.Text);
+                    double width = double.Parse(txtvalue2.Text);
+                    shape = new Rectangle(length, width);
+                }
+                else if (cmbshapeType.SelectedItem.ToString() == "دایره")
+                {
+                    double radius = double.Parse(txtvalue1.Text);
+                    shape = new Circle(radius);
+                }
+                if (shape!=null)
+                {
+                    double area = shape.CalculateArea();
+                    lbresult.Text = $"مساحت: {area:F2}";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"خطا: {ex.Message}", "خطا", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+    public  abstract class Shape
+    {
+    public abstract double CalculateArea();
+    }
+    public class Circle : Shape
+    {
+    public double Radius { get; set; }
+     public Circle (double radius)
+    {
+        Radius = radius;
+    }
+    public override double CalculateArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+   }
+    public class Rectangle : Shape
+    {
+     public double Lenght { get; set; }
+     public double Width { get; set; }
+    public Rectangle (double length, double width)
+    {
+        Lenght = length;
+        Width = width;
+    }
+    public override double CalculateArea()
+    {
+        return Lenght * Width;
+    }
+}  
+
+
